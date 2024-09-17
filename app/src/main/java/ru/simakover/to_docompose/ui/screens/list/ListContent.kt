e47e6c842fd46149e2afce8f87df8dfded962ffc
sedavnyhna <sedavnyhna@skblab.ru>
2024-09-18 01:44:38 +0500

    implement list of items

--- app/src/main/java/ru/simakover/to_docompose/ui/screens/list/ListContent.kt
+++ app/src/main/java/ru/simakover/to_docompose/ui/screens/list/ListContent.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,8 +28,23 @@ import ru.simakover.to_docompose.ui.theme.LARGE_PADDING
 import ru.simakover.to_docompose.ui.theme.PRIORITY_INDICATOR_SIZE
 
 @Composable
-fun ListContent() {
-
+fun ListContent(
+    tasks: List<ToDoTask>,
+    navigateToTaskScreen: (taskId: Int) -> Unit
+) {
+    LazyColumn{
+        items(
+            items = tasks,
+            key = { task ->
+                task.id
+            }
+        ) { task ->
+            TaskItem(
+                toDoTask = task,
+                navigateToTaskScreen = navigateToTaskScreen
+            )
+        }
+    }
 }
 
 @Composable
--- app/src/main/java/ru/simakover/to_docompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/ru/simakover/to_docompose/ui/screens/list/ListScreen.kt
@@ -1,13 +1,20 @@
 package ru.simakover.to_docompose.ui.screens.list
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.room.util.TableInfo
 import ru.simakover.to_docompose.R
 import ru.simakover.to_docompose.ui.viewmodels.SharedViewModel
 import ru.simakover.to_docompose.util.SearchAppBarState
@@ -17,12 +24,27 @@ fun ListScreen(
     navigateToTaskScreen: (taskId: Int) -> Unit,
     sharedViewModel: SharedViewModel
 ) {
+    LaunchedEffect(key1 = true) {
+        sharedViewModel.getAllTasks()
+    }
+    val allTasks by sharedViewModel.allTasks.collectAsState()
+
     val searchAppBarState: SearchAppBarState by sharedViewModel.searchAppBarState
     val searchTextState: String by sharedViewModel.searchTextState
 
     Scaffold(
-        content = {
-            ListContent()
+        modifier = Modifier.fillMaxSize(),
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                ListContent(
+                    tasks = allTasks,
+                    navigateToTaskScreen = navigateToTaskScreen
+                )
+            }
         },
         topBar = {
             ListAppBar(
