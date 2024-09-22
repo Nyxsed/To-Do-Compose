0d030124e3e7d56a6192ba7d950234ee567f935e
sedavnyhna <sedavnyhna@skblab.ru>
2024-09-22 13:21:28 +0500

    snackbar implementation

--- app/src/main/java/ru/simakover/to_docompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/ru/simakover/to_docompose/ui/screens/list/ListScreen.kt
@@ -8,15 +8,21 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.room.util.TableInfo
+import kotlinx.coroutines.launch
 import ru.simakover.to_docompose.R
 import ru.simakover.to_docompose.ui.viewmodels.SharedViewModel
+import ru.simakover.to_docompose.util.Action
 import ru.simakover.to_docompose.util.SearchAppBarState
 
 @Composable
@@ -34,9 +40,18 @@ fun ListScreen(
     val searchAppBarState: SearchAppBarState by sharedViewModel.searchAppBarState
     val searchTextState: String by sharedViewModel.searchTextState
 
-    sharedViewModel.handleDatabaseActions(action)
+//    sharedViewModel.handleDatabaseActions(action)
+
+    val snackBarHostState = remember { SnackbarHostState() }
+    DisplaySnackBar(
+        snackbarHostState = snackBarHostState,
+        handeDataBaseActions = { sharedViewModel.handleDatabaseActions(action) },
+        taskTitle = sharedViewModel.title.value,
+        action = sharedViewModel.action.value
+    )
 
     Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
         modifier = Modifier.fillMaxSize(),
         content = { innerPadding ->
             Column(
@@ -76,5 +91,26 @@ fun ListFab(
             imageVector = Icons.Filled.Add,
             contentDescription = stringResource(R.string.add_button)
         )
+    }
+}
+
+@Composable
+fun DisplaySnackBar(
+    snackbarHostState: SnackbarHostState,
+    handeDataBaseActions: () -> Unit,
+    taskTitle: String,
+    action: Action
+) {
+    handeDataBaseActions()
+
+    val scope = rememberCoroutineScope()
+    LaunchedEffect(key1 = action) {
+        if (action != Action.NO_ACTION) {
+            scope.launch {
+                val snackBarResult = snackbarHostState.showSnackbar(
+                    message = "${action.name}. $taskTitle", "OK", duration = SnackbarDuration.Short
+                )
+            }
+        }
     }
 }
